bc1e9e881986efd3a501d046d49a1903b508cb0e
Pejantantangguh <suherman.ng@gmail.com>
2018-12-19 13:48:01 +1000

    Linear_interpolation_done

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 const canvasSketch = require('canvas-sketch');
+const {lerp} = require ('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [2048, 2048]
  
 };
 
+const margin = 100;
+
 
 
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 70;
     for(let x = 0; x < count; x++) {
       for(let y = 0; y < count ; y++ ){
         const u = count <=1 ?  0.5 : x / ( count - 1 );
@@ -21,20 +25,22 @@ const sketch = () => {
     return points;
   };
 
-  const points = createGrid();
-  console.log(points);
+  random.setSeed();
+  const points = createGrid().filter( () => random.value() > 0.5 );
+  // console.log(points);
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect (0,0,width,height);
 
     points.forEach(([u,v]) => {
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - marginasdasd
 
       context.beginPath();
-      context.arc(x,y,100,0,Math.PI * 2, false);
-      context.strokeStyle = 'black';
+      context.arc(x,y,5,0,Math.PI * 2, false);
+          context.strokeStyle = 'black';
+      
       context.lineWidth = 10;
       context.stroke();
     });
